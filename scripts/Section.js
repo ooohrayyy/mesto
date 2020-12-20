b39fe68f870c7d69cb0bc208f369a1cc3deba662
Boris Nikitashov <ooohrayyy@gmail.com>
2020-12-20 18:33:57 +0300

    refactor: Внести правки в Section

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,23 +1,17 @@
 class Section {
-  constructor ( {data, renderer}, containerSelector ) {
-    this._renderedItems = data;
+  constructor ( {items, renderer}, containerSelector ) {
+    this._renderedItems = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
-  addItem (element) {
-    this._container.append(element);
-  }
-
-  clear () {
-    this._container.innerHTML = '';
-  }
-
-  renderItems () {
-    this.clear();
-
+  _renderItems () {
     this._renderedItems.forEach(item => {
       this._renderer(item);
     });
+  }
+
+  addItem (element) {
+    this._container.append(element);
   }
 }
