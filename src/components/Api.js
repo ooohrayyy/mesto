c05c75bf63efd48080a34700b2c31b447af9542c
Boris Nikitashov <ooohrayyy@gmail.com>
2021-01-15 19:43:11 +0300

    feat: Добавить метод getUserInfo в Api

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,4 +3,12 @@ export default class Api {
     this._baseUrl = options.baseUrl;
     this._headers = options.headers;
   }
+
+  getUserInfo () {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this._headers
+    })
+      .then(res => res.json());
+  }
 }
