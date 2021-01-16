289bcd8b357d235d5b96c21e525c14e20d61f81e
Boris Nikitashov <ooohrayyy@gmail.com>
2021-01-16 20:35:16 +0300

    refactor: Переименовать класс popup__save

--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,7 @@
 const validationConfig = { // Конфигурация валидации
   formSelector: '.popup__container',
   inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save',
+  submitButtonSelector: '.popup__button',
   inputErrorSelector: '.popup__error',
   activeErrorClass: 'popup__error_active',
   invalidInputClass: 'popup__input_invalid'
