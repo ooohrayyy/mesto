10568cc18895393c9023fe0b1061199f24e719f6
Boris Nikitashov <ooohrayyy@gmail.com>
2021-01-19 14:49:32 +0300

    style: Добавить комментарий

--- src/components/Api.js
+++ src/components/Api.js
@@ -7,7 +7,7 @@ export default class Api {
     this.toggleLike = this.toggleLike.bind(this);
   }
 
-  _checkResponseData (res) {
+  _checkResponseData (res) { // Проверка ответа от сервера
     if (!res.ok) {
       return Promise.reject(`Ошибка: ${res.status}`);
     }
